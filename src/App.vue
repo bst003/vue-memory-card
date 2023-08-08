8a735defd524d6523e5ea209bee96dd4fa2cf9ce
bst003 <bstanner33@yahoo.com>
2023-08-08 16:12:46 -0400

    Set up methods to query poke api

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,10 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
-import { onMounted } from 'vue'
 
 import Header from './components/Header.vue';
 import GameBody from './components/GameBody.vue';
 
-const formatPokemonData = (data) => {
-        const formattedData = {
-            id: data.id,
-            name: data.name,
-            sprite: data.sprites.front_default,
-        };
 
-        return formattedData;
-    };
-
-    const shuffle = (array) => {
-        array.sort((a, b) => {
-            return Number(Math.random() - 0.5);
-        });
-
-        return array;
-    };
-
-onMounted(() => {
-  const getPokemonData = async (id) => {
-            try {
-                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-
-                const unformattedPokemonData = await response.json();
-
-                const formattedPokemonData = formatPokemonData(unformattedPokemonData);
-
-                return formattedPokemonData;
-            } catch (err) {
-                console.log("Error fetching pokemon");
-                console.error(err);
-            }
-        };
-
-        const createInitialPokeArr = async () => {
-            let initialPokeArr = [];
-
-            for (let i = 0; i <= 11; i++) {
-                const pokemonData = await Promise.resolve(getPokemonData(i + 1));
-
-                initialPokeArr.push(pokemonData);
-            }
-
-            initialPokeArr = shuffle(initialPokeArr);
-
-            return initialPokeArr;
-        };
-
-        const fillPokeArrState = async () => {
-            const initialPokeArr = await createInitialPokeArr();
-
-            console.log(initialPokeArr);
-
-        };
-
-        fillPokeArrState();
-});
 </script>
 
 <template>
--- src/components/GameBody.vue
+++ src/components/GameBody.vue
@@ -1,4 +1,66 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+
+const pokeArrState = ref([]);
+
+const formatPokemonData = (data) => {
+        const formattedData = {
+            id: data.id,
+            name: data.name,
+            sprite: data.sprites.front_default,
+        };
+
+        return formattedData;
+    };
+
+    const shuffle = (array) => {
+        array.sort((a, b) => {
+            return Number(Math.random() - 0.5);
+        });
+
+        return array;
+    };
+
+onMounted(() => {
+  const getPokemonData = async (id) => {
+            try {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+                const unformattedPokemonData = await response.json();
+
+                const formattedPokemonData = formatPokemonData(unformattedPokemonData);
+
+                return formattedPokemonData;
+            } catch (err) {
+                console.log("Error fetching pokemon");
+                console.error(err);
+            }
+        };
+
+        const createInitialPokeArr = async () => {
+            let initialPokeArr = [];
+
+            for (let i = 0; i <= 11; i++) {
+                const pokemonData = await Promise.resolve(getPokemonData(i + 1));
+
+                initialPokeArr.push(pokemonData);
+            }
+
+            initialPokeArr = shuffle(initialPokeArr);
+
+            return initialPokeArr;
+        };
+
+        const fillPokeArrState = async () => {
+            const initialPokeArr = await createInitialPokeArr();
+
+            console.log(initialPokeArr);
+
+            pokeArrState.value = [...initialPokeArr];
+        };
+
+        fillPokeArrState();
+});
 
 </script>
 
@@ -11,7 +73,9 @@
             </p>
 
             <div className="gameCards-grid">
-
+                <li v-for="pokemon in pokeArrState">
+  {{ pokemon.name }}
+</li>
             </div>
      </section>
 </template>
